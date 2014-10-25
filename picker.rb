a3d64e30969f302d1fdcb2edad250e07f07bfc47
KaNoKWaNN <tatsananipan@hotmail.com>
2014-10-25 14:14:03 -0300

    Update picker.rb

--- picker.rb
+++ picker.rb
@@ -1,5 +1,5 @@
-def random_name_engineer
+def random_name_engineer()
 	name = File.readlines("name_engineer.txt")
 	name[rand(name.length)]
 end
-puts random_name_engineer
+puts random_name_engineer()
